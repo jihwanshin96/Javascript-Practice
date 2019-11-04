d0f85b6e7300cf1ec7f4d10c67332f82e729f75f
jihwanshin96 <54863676+jihwanshin96@users.noreply.github.com>
2019-11-04 14:15:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+//01-stringCompare.js//
+
+if ('ABC' > 'AB'){
+  alert('true')
+} else {
+  alert('false')
+}
+
+//02-numberCompare.js//
+
+var num = 15;
+
+if (num === 25){
+  alert('correct!')
+} else if (num > 25){
+  alert('too large!')
+} else{
+  alert('please try again')
+}
+
+//03-functionCombine.js//
+
+var firstName = "Eric";
+var lastName = "Shin";
+var space = " ";
+
+function fullName() {
+  console.log(firstName + space +lastName);
+}
+
+fullName();
+
+//04-functionConditions.js//
+
+function person(num){
+  if (num === 1){
+    return "Taylor Swift";
+  } else if (num === 2){
+    return "Ariana Grande";
+  } else if (num === 3){
+    return "Miley Cyrus"
+  } else {
+    return "Mariah Carey because she is the Queen of Christmas"
+  }
+}
+
+console.log(person(1));
+console.log(person(2));
+console.log(person(3));
+console.log(person(4));
+
+//05-randomWords.js//
+
+var firstArray = ["sexy","awful","gross","gorgeous","enormous"];
+var secondArray = ["pancakes","handcuffs","carousels","toast","giraffes"];
+
+var randomIndex = (firstArray.random() + " " + secondArray.random());
+console.log(randomIndex);
